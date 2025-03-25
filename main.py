43a85b8065a3d3f45871ee3e5cc6046d7f53fb6d
Shaxriyorboy <shaxriyortursunaliyev17@gmail.com>
2025-03-25 05:19:03 +0500

    edit main

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from book_routes import book_router
 from unit_routes import unit_router
 from word_routes import word_router
+from database import Base, engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(book_router)
 app.include_router(unit_router)
