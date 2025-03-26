2e16247c53e04faa3c290c50ac94a711d5b6a076
Shaxriyorboy <shaxriyortursunaliyev17@gmail.com>
2025-03-26 09:36:13 +0500

    fix favorite words

--- word_routes.py
+++ word_routes.py
@@ -57,6 +57,35 @@ async def add_new_word(word: WordModel):
 
     return jsonable_encoder(custom_data)
 
+@word_router.get('/favorite-words',status_code=200)
+async def get_favorite_words():
+    
+    words = session.query(Word).filter(Word.isFavorite==True).all()
+
+    custom_data = {
+        'success':True,
+        'code': 200,
+        'message': 'Hammasi yaxshi',
+        'data': [
+            {
+                'id': word.id,
+                "word_en": word.word_en,
+                'word_uz': word.word_uz,
+                'comment': word.comment,
+                'definition': word.definition,
+                'phonetic': word.phonetic,
+                'example': word.example,
+                'word_classes': word.word_classes,
+                'isFavorite': word.isFavorite,
+                'unit_id': word.unit_id
+            }
+            for word in words
+        ]
+    }
+    return jsonable_encoder(custom_data)
+
+
+
 @word_router.get('/{unit_id}',status_code=200)
 async def get_unit_words(unit_id:int):
     unit = session.query(Unit).filter(Unit.id == unit_id).first()
@@ -93,32 +122,6 @@ async def get_unit_words(unit_id:int):
     return jsonable_encoder(custom_data)
 
 
-@word_router.get('/favorite-words',status_code=200)
-async def get_favorite_words():
-    
-    words = session.query(Word).filter(Word.isFavorite==True).all()
-
-    custom_data = {
-        'success':True,
-        'code': 200,
-        'message': 'Hammasi yaxshi',
-        'data': [
-            {
-                'id': word.id,
-                "word_en": word.word_en,
-                'word_uz': word.word_uz,
-                'comment': word.comment,
-                'definition': word.definition,
-                'phonetic': word.phonetic,
-                'example': word.example,
-                'word_classes': word.word_classes,
-                'isFavorite': word.isFavorite,
-                'unit_id': word.unit_id
-            }
-            for word in words
-        ]
-    }
-    return jsonable_encoder(custom_data)
 
 @word_router.put('/add-favorite/{id}',status_code=200)
 async def add_favorite_words(id: int):
